85e9b68d79bf65c49552915dbaef69e4a2c8c408
MyStraw <kikongdosa@gmail.com>
2023-04-07 18:00:26 +0900

    objective2

--- src/com/ruby/java/ch06/ArmorTest2.java
+++ src/com/ruby/java/ch06/ArmorTest2.java
@@ -10,3 +10,11 @@ public class ArmorTest2 {
 		System.out.println(suit.getName() + " : " + suit.getHeight());
 	}
 }
+
+//에러메세지
+//Exception in thread "main" java.lang.NullPointerException: Cannot invoke "com.ruby.java.ch06.Armor.setName(String)" because "suit" is null
+// at com.ruby.java.ch06.ArmorTest2.main(ArmorTest2.java:8)
+
+//참조변수가 null. ArmorTest2의 8번째 라인 부분이 문제.
+//null.setName() 하니까 null은 가리키는게 없는데?
+//윗줄 null pointer 예외. 부를라 하는데 없으니 에러. 가장 빈번한 오류다.
---
+++ src/com/ruby/java/ch06/Count.java
@@ -0,0 +1,8 @@
+package com.ruby.java.ch06;
+
+public class Count {
+	
+	public static int totalCoung; // 파일 떼어낼때는 퍼블릭을 붙여 카운터 테스트 위에 있는거
+	int count;
+
+}
