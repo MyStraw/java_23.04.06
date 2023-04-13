441f1bd7da1df4e38af36db43bf755d3b577e580
MyStraw <kikongdosa@gmail.com>
2023-04-13 22:25:23 +0900

    집이랑 연동

--- src/chapter7/mission8.java
+++ src/chapter7/mission8.java
@@ -14,22 +14,28 @@ import java.util.Scanner;
 public class mission8 {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 
-		while(true) {
-		System.out.println("얼마?");
-		Scanner sc = new Scanner(System.in);		
-		int money = sc.nextInt(); // 배열루프써보자
+		while (true) {
+			System.out.println("얼마?");
+			Scanner sc = new Scanner(System.in);
+			int money = sc.nextInt(); // 배열루프써보자
 
-		// 배열 써서 나누기 하고 나머지를 안에 집어넣기.
-		// 이걸 계속
+			// 배열 써서 나누기 하고 나머지를 안에 집어넣기.
+			// 이걸 계속
 
-		int oman = money / 50000; // int 잖아! 정수
-				
-		System.out.println(money / 50000);
-		System.out.println(money % 50000/10000);
-		System.out.println(money % 50000 % 10000 / 5000);
-		System.out.println(money % 50000 % 10000 % 5000 / 1000);
+			int oman = money / 50000; // int를 써서 나머지를 버려버린다.
+			int omans = money % 50000; // 나머지를 받아온다
+			int ilman = omans / 10000;
+			int ilmans = omans % 10000;
+			int ochun = ilmans / 5000;
+			int ochuns = ilmans % 5000;
+			int ilchun = ochuns / 1000;
+			int ilchuns = ochuns % 1000;
+
+			System.out.println(money / 50000);
+			System.out.println(money % 50000 / 10000);
+			System.out.println(money % 50000 % 10000 / 5000);
+			System.out.println(money % 50000 % 10000 % 5000 / 1000);
 //
 //		int omans = money % 50000;
 //		int ilman = omans / 10000;
@@ -44,60 +50,36 @@ public class mission8 {
 //		System.out.println("일만원은 " + ilman + "장 필요해용");
 //		System.out.println("오천원은 " + ochun + "장 필요해용");
 //		System.out.println("일천원은 " + ilchun + "장 필요해용");
-		
-	
-		
-		
-		
 
-		}
+			System.out.println("오만원은 " + oman + "장 필요해용");
+			System.out.println("일만원은 " + ilman + "장 필요해용");
+			System.out.println("오천원은 " + ochun + "장 필요해용");
+			System.out.println("일천원은 " + ilchun + "장 필요해용");
 
-	}
-}
-=======
+			System.out.println("-------------------------------------------");
 
-		System.out.println("얼마?");
-		Scanner sc = new Scanner(System.in);
-		int money = sc.nextInt(); // 배열루프써보자
+			System.out.println(money / 50000);
+			System.out.println(money % 50000 / 10000);
+			System.out.println(money % 50000 % 10000 / 5000);
+			System.out.println(money % 50000 % 10000 % 5000 / 1000);
 
-		int oman = money / 50000; // int를 써서 나머지를 버려버린다.
-		int omans = money % 50000; // 나머지를 받아온다
-		int ilman = omans / 10000;
-		int ilmans = omans % 10000;
-		int ochun = ilmans / 5000;
-		int ochuns = ilmans % 5000;
-		int ilchun = ochuns / 1000;
-		int ilchuns = ochuns % 1000;
-		
-		System.out.println("오만원은 " + oman + "장 필요해용");
-		System.out.println("일만원은 " + ilman + "장 필요해용");
-		System.out.println("오천원은 " + ochun + "장 필요해용");
-		System.out.println("일천원은 " + ilchun + "장 필요해용");
-		
-		System.out.println("-------------------------------------------");
-		
-		System.out.println(money / 50000);
-		System.out.println(money % 50000 / 10000);
-		System.out.println(money % 50000 % 10000 / 5000);
-		System.out.println(money % 50000 % 10000 % 5000 / 1000);
+			System.out.println("-------------------------------------------");
 
-		System.out.println("-------------------------------------------");
-		
 //		int[] cash = new int[4];
 //		cash[0]= 50000;
 //		cash[1]= 10000;
 //		cash[2]= 5000;
 //		cash[3]= 1000;
 
-		int[] cash = { 50000, 10000, 5000, 1000 };
-		for (int i : cash) { // i에 배열을 순차적으로 입력
-			int quo = money / i;
-			System.out.println(i + "원짜리 " + quo + "장 필요해");
-			money = money % i;
+			int[] cash = { 50000, 10000, 5000, 1000 };
+			for (int i : cash) { // i에 배열을 순차적으로 입력
+				int quo = money / i;
+				System.out.println(i + "원짜리 " + quo + "장 필요해");
+				money = money % i;
+
+			}
 
 		}
 
 	}
-
 }
->>>>>>> fd8fa7ce7f041d2b3bd012f92b54a0d54526b9f2
