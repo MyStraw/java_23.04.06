fd8fa7ce7f041d2b3bd012f92b54a0d54526b9f2
MyStraw <kikongdosa@gmail.com>
2023-04-13 20:06:15 +0900

    스터디1일차끝

--- src/study/day1_1.java
+++ src/study/day1_1.java
@@ -4,6 +4,7 @@ public class day1_1 {
 	public static void main(String[] args) {
 
 		for (int i = 2; i < 10; i++) {
+								
 			for (int k = 1; k < 10; k++) {
 					
 				System.out.println(i + " X " + k + " = " + i * k);			
