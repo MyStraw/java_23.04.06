3f9d856bea2b6a72012bebd5087032b7274d28c8
MyStraw <kikongdosa@gmail.com>
2023-04-13 22:19:22 +0900

    이전꺼?

---
+++ bin/README.md
@@ -0,0 +1,2 @@
+# K-digital 3기 Java 수업
++ 04.05 GitHub 연동시작
--- src/chapter7/mission8.java
+++ src/chapter7/mission8.java
@@ -2,6 +2,7 @@ package chapter7;
 //최소 지폐 수 계산
 
 //상품가격 152,365원을 지불하기 위해 필요한 최소 지폐 장수는 아래와 같다.
+
 //5만원 * 3장
 //1만원 * 0장
 //5천원 * 0장
@@ -12,33 +13,43 @@ import java.util.Scanner;
 
 public class mission8 {
 
-	Scanner sc = new Scanner(System.in);
-	int money = sc.nextInt(); // 배열루프써보자
+	public static void main(String[] args) {
+
+		while(true) {
+		System.out.println("얼마?");
+		Scanner sc = new Scanner(System.in);		
+		int money = sc.nextInt(); // 배열루프써보자
+
+		// 배열 써서 나누기 하고 나머지를 안에 집어넣기.
+		// 이걸 계속
+
+		int oman = money / 50000; // int 잖아! 정수
+				
+		System.out.println(money / 50000);
+		System.out.println(money % 50000/10000);
+		System.out.println(money % 50000 % 10000 / 5000);
+		System.out.println(money % 50000 % 10000 % 5000 / 1000);
+//
+//		int omans = money % 50000;
+//		int ilman = omans / 10000;
+//		int ilmans = omans % 10000;
+//		int ochun = ilmans / 5000;
+//		int ochuns = ilmans % 5000;
+//		int ilchun = ochuns / 1000;
+//		int ilchuns = ochuns % 1000;
+//		
+//
+//		System.out.println("오만원은 " + oman + "장 필요해용");
+//		System.out.println("일만원은 " + ilman + "장 필요해용");
+//		System.out.println("오천원은 " + ochun + "장 필요해용");
+//		System.out.println("일천원은 " + ilchun + "장 필요해용");
+		
 	
-	
-	//배열 써서 나누기 하고 나머지를 안에 집어넣기.
-	//이걸 계속 
+		
+		
+		
 
+		}
 
-	
-	
-	int oman = money / 50000; // int 잖아! 정수
-
-	int omans = money % 50000;
-
-	int ilman = omans / 10000;
-
-	int ilmans = omans % 10000;
-
-	int ochun = ilmans / 5000;
-
-	int ochuns = ilmans % 5000;
-
-	int ilchun = ochun / 1000;
-
-	int ilchuns = ochuns % 1000;
-
-
-System.out.println("오만원은 "+oman+"장 필요해용");
-
+	}
 }
