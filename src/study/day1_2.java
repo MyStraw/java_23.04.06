fde9f484ac80755e40135c7b200154caa0f94909
MyStraw <kikongdosa@gmail.com>
2023-04-13 19:57:06 +0900

    스터디1일차

---
+++ src/study/day1_2.java
@@ -0,0 +1,16 @@
+package study;
+
+public class day1_2 {
+
+	public static void main(String[] args) {
+
+		for (int i = 0; i < 11; i++) {
+			for (int k = 1; k < i + 1; k++) {
+				System.out.print(" ");
+			}
+			System.out.print("*");
+			System.out.println();
+
+		}
+	}
+}
