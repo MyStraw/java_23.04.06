50fe34baa4be4355a70f13722ace27284d7e1d30
MyStraw <kikongdosa@gmail.com>
2023-04-17 22:30:28 +0900

    좀 땡겨오자

---
+++ src/chapter10/Test01.java
@@ -0,0 +1,90 @@
+package chapter10;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class Employee {
+	String name;
+	int age;
+	int height;
+	int weight;
+	char bloodType;
+	String email;
+
+	public Employee(String name, int age, int height, int weight, char bloodType, String email) { // 생성자
+		this.name = name;
+		this.age = age;
+		this.height = height;
+		this.weight = weight;
+		this.bloodType = bloodType;
+		this.email = email;
+	}
+
+	public String toString() {// 출력형식 폼
+		return name + ", " + age + ", " + height + ", " + weight + ", " + bloodType + ", " + email;
+	}
+}
+
+class Student {
+	int sid;
+	String name;
+}
+
+public class Test01 {
+	public static void main(String[] args) {
+		ArrayList<String> list = new ArrayList<String>(); // 여기 보라색이 제네릭 리스트. int가들어가면 int.
+
+		list.add("서울");
+		list.add("부산");
+		list.add("창원");
+		list.add("서울");
+		list.add("울산");
+		list.add("밀양");
+//		for (int i = 0; i < list.size(); i++)
+//			System.out.print(" " + list.get(i).toString());
+		System.out.println(list.toString());
+		list.add(0, "인천"); // 0번째에 인천을 끼운다.
+		list.remove(1);// 인덱스 1번(서울) 지운다.
+		System.out.println("\n인천 삽입후::");
+//		for (int i = 0; i < list.size(); i++)
+//			System.out.print(" " + list.get(i).toString());
+//		System.out.println();
+		System.out.println(list.toString()); // 실행하니까 배열 쭉 보여준다.
+		Object obj[] = list.toArray();// 471p 27line//리스트를 배열로 만들고
+		System.out.println("8:: " + Arrays.toString(obj));//배열을 출력.
+		
+
+//		ArrayList<Student> list = new ArrayList<>(); //여기 보라색이 제네릭 리스트. 뒤 보라는 생략가능. 467p
+		// <Student>를 없애면 무슨 타입이든지 다 들어갈수가 있게된다.
+
+		Object arr[] = new Object[10]; // 이건 배열~
+		ArrayList lst = new ArrayList();
+		String s = "java";
+		Integer i = 123;
+		arr[0] = s;
+		arr[1] = i;
+		int m = arr.length;
+		int l = s.length();
+//	int len = arr[0].length();// arr[0]이 스트링이야. 이것만왜 에러? 467p. [0]뒤에 . 찍어보면 length없다. 부모에 없다.
+		lst.add(s);
+		lst.add(i);
+		int h = ((String) lst.get(0)).length();// 타입변환
+//	int []data = new int [10];//이런 예들은 에러 안나는디
+//	int k = data.length;
+	}
+
+//		int[] a = new int[10];
+//		Employee[] eset = new Employee[10];
+//		eset[0] = new Employee("hong", 11, 130, 50, 'A', "bhhong@go.kr");
+//		eset[1] = new Employee("kim", 11, 130, 50, 'A', "bhhong@go.kr");
+//		eset[2] = new Employee("lee", 11, 130, 50, 'A', "bhhong@go.kr");
+//		eset[3] = new Employee("park", 11, 130, 50, 'A', "bhhong@go.kr");
+//		eset[4] = eset[3];
+//		eset[3] = eset[2]; //이런과정을 해줘야중간에 끼어든다. 근데 이러면 노가다고 이런코딩 하믄 안돼. 배열을 해야된다.
+//		eset[2] = new Employee("choi", 11, 130, 50, 'A', "bhhong@go.kr"); // 이러면 기존의 2가 날라가.
+//		for (int i = 0; i < 4; i++) {
+//			System.out.println(eset[i].toString());
+//		}
+//		eset[4] = new Employee("choi", 11, 130, 50, 'A', "bhhong@go.kr"); // 이걸2번째에 넣고싶다~ 위에 2 넣으면 기존이날라가.
+// 	}
+}
--- src/chapter9/Test02.java
+++ src/chapter9/Test02.java
@@ -1,30 +1,55 @@
 package chapter9;
 
+import java.util.Arrays;
+
 public class Test02 {
-	
+
 	public static void main(String[] args) {
-		
-		String s1 = new String("hello");
-		String s2 = "hello"; //값은 같은데 객체의 
-		System.out.println(System.identityHashCode(s1) + "::" + System.identityHashCode(s2));//식별자가다르다
-		System.out.println(s1.toString() + "::" + s2.toString());
-		System.out.println(s1.hashCode() + "::" + s2.hashCode());
-		if(s1==s2)
-			System.out.println("s1 == s2");
-		else
-			System.out.println("같지 않다");
-		
+
+		String s1 = new String("java");
+		String s2 = "java"; // 값은 같은데 객체의
 		String s3 = new String("java");
 		String s4 = "java";
-		
-		if(s1 ==s3) {
-			System.out.println("동일 객체이다.");
-		} else {
-			System.out.println("다른 객체이다.");
-		}
-		if(s2 == s4) {
-			System.out.println("동일 객체이다.");
-		}
-	}
 
+		System.out.println("s1: " + s1.hashCode() + " s3: " + s3.hashCode());
+
+		if (s1.equals(s3))
+			System.out.println("s1 equals s3");
+		else
+			System.out.println("s1 not equals s3");
+		if (s1 == s3)
+			System.out.println("s1 == s3");
+		else
+			System.out.println("같지 않다");
+
+		System.out.println("------------------------------------");
+		if (s2.equals(s4))
+			System.out.println("s2 equals s4");
+		else
+			System.out.println("s2 not equals s4");
+
+		if (s2 == s4)
+			System.out.println("동일 객체이다.");
+		else
+			System.out.println("같지 않다");
+
+		System.out.println("------------------------------------");
+		if (s1.equals(s2))
+			System.out.println("s1 equals s2");
+		else
+			System.out.println("s1 not equals s2");
+
+		if (s1 == s2)
+			System.out.println("동일 객체이다.");
+		else
+			System.out.println("같지 않다");
+		System.out.println("------------------------------------");
+
+		System.out.println(System.identityHashCode(s1) + "::" + System.identityHashCode(s2));// 식별자가다르다
+		System.out.println(s1.toString() + "::" + s2.toString());
+		System.out.println(s1.hashCode() + "::" + s2.hashCode());
+		
+		int i3 = Integer.parseInt("123"); //문자열123을 정수 123으로 바꿔준다
+		
+	}
 }
