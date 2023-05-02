356ae739cab7376cffe8775ed8e2feac66196cdf
MyStraw <kikongdosa@gmail.com>
2023-05-02 18:01:46 +0900

    5월2일

---
+++ src/study/_4_9_바구니뒤집기.java
@@ -0,0 +1,21 @@
+package study;
+
+import java.util.Scanner;
+
+public class _4_9_바구니뒤집기 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		int N[] = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			N[i] = i + 1;
+		}
+
+		int M = sc.nextInt();
+
+	}
+
+}
