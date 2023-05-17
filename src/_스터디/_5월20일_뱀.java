f07e210652be394e9780e3574b6eb4dd5a1f94d1
MyStraw <kikongdosa@gmail.com>
2023-05-17 11:47:22 +0900

    뱀 추가

---
+++ src/_스터디/_5월20일_뱀.java
@@ -0,0 +1,16 @@
+package _스터디;
+
+import java.util.Scanner;
+
+public class _5월20일_뱀 {
+	
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int N = sc.nextInt();
+		
+				
+	}
+
+}
