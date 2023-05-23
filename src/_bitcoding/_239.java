69729c4faf5db9d8ff3486ad725a2a117fe25c9c
MyStraw <kikongdosa@gmail.com>
2023-05-23 18:09:51 +0900

    5월 23일 끝

---
+++ src/_bitcoding/_239.java
@@ -0,0 +1,26 @@
+package _bitcoding;
+
+public class _239 {
+	public static int sum1(int n) {
+		if (n==1) {			
+			return 1;
+		}
+		else {
+			
+			return (n + sum1(n-1));			
+		}		
+	}
+
+	public static int sum2(int n) {
+		int sum = 0;
+		for(int i = 1; i<= n; i++) {
+			sum += i;
+		}
+		return sum;			
+	}
+	
+	public static void main(String[] args) {
+		System.out.println(sum1(4));
+		
+	}
+}
