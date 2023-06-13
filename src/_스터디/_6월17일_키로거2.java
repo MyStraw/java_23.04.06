8210a13520e8ae0da1c422fcce63ee1584b82274
MyStraw <kikongdosa@gmail.com>
2023-06-14 00:55:40 +0900

    키로거2

---
+++ src/_스터디/_6월17일_키로거2.java
@@ -0,0 +1,50 @@
+package _스터디;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class _6월17일_키로거2 {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine()); // 케이스 개수
+
+//		StringBuilder sb = new StringBuilder();		
+//		Scanner sc = new Scanner(System.in);
+//		int N = sc.nextInt();		
+
+		boolean k = true;
+
+		Stack<Character> keyloger = new Stack<>();
+		Stack<Character> back = new Stack<>();
+
+		for (int i = 0; i < N; i++) {
+			String L = br.readLine();
+			keyloger.clear();
+			back.clear();
+
+			for (char c : L.toCharArray()) {
+				keyloger.push(c);
+
+				switch (keyloger.peek()) {
+				case '<':
+					keyloger.pop();
+					back.push(keyloger.pop());					
+					break;
+				case '>':
+					keyloger.pop();
+					break;
+				case '-':
+					keyloger.pop();
+					keyloger.pop();
+					break;
+				}
+				if (!keyloger.isEmpty())
+					System.out.println(keyloger.peek());
+			}
+		}
+	}
+
+}
