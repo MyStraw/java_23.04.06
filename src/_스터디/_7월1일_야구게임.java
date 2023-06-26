d736d08528a7cb13b0988b62ffc301ace8537e63
MyStraw <kikongdosa@gmail.com>
2023-06-26 17:23:57 +0900

    야구

---
+++ src/_스터디/_7월1일_야구게임.java
@@ -0,0 +1,18 @@
+package _스터디;
+
+public class _7월1일_야구게임 {
+	public static void main(String[] args) {
+		
+		int s = 123;
+		
+		String f = "123";
+		
+		
+		
+		
+		System.out.println(f.charAt(0));		
+		System.out.println(f.contains("1"));
+		
+		
+	}
+}
