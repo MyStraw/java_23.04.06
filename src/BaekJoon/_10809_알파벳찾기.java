3d794539da5dea7a1511f21d6396474552604914
MyStraw <kikongdosa@gmail.com>
2023-07-13 17:49:01 +0900

    문제풀기

---
+++ src/BaekJoon/_10809_알파벳찾기.java
@@ -0,0 +1,22 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class _10809_알파벳찾기 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String str = sc.next();
+		
+		String alpha = "abcdefghijklmnopqrstuvwxyz";
+		
+		for(int i = 0 ; i<str.length() ; i++) {
+			
+				
+			}
+				
+		}
+		
+		
+	}
+	
