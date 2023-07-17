d2e6dc8c3c895e5dd3c8acbb799571e502e0e5eb
MyStraw <kikongdosa@gmail.com>
2023-07-17 17:40:14 +0900

    7월17일

--- src/BaekJoon/_1157_단어공부.java
+++ src/BaekJoon/_1157_단어공부.java
@@ -1,22 +1,49 @@
 package BaekJoon;
 
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 public class _1157_단어공부 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-
-		StringBuilder sb = new StringBuilder();
 		String s = sc.next();
-		sb.append(s);
+		String alpha = "abcdefghijklmnopqrstuvwxyz";
 
-		System.out.println(s.toUpperCase());
+		char[] str = s.toLowerCase().toCharArray();
+		char[] alphabet = alpha.toCharArray();
 
-	}
+		int[] countt = new int[str.length];
 
-	public static void boool(int i, String s) {
-		for (int k = i ; k < s.length(); k++) {
-						
+		int count = 0;
+		
+		for (int i = 0; i < str.length-1; i++) {		
+			for (int j = 1; j < str.length; j++) {
+				if (str[i] == str[j]) {
+					count++;
+				}
+			}
+		}
+
+		int countmax = 0;
+		int countjung = 0;
+		for (int i = 0; i < countt.length; i++) {
+			if (countmax < countt[i]) {
+				countmax = countt[i];
+			}
+		}
+
+		for (int i = 0; i < countt.length; i++) {
+			if (countmax == countt[i]) {
+				countjung++;
+			}
+		}
+
+		if (countjung >= 2) {
+			System.out.println("?");
+		} else {
+			System.out.println(countmax);
+
 		}
 	}
 }
