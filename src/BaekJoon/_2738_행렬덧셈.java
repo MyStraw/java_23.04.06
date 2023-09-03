1aa51b53e5cad6b900f9ba098320d910598990b8
MyStraw <kikongdosa@gmail.com>
2023-09-04 01:00:09 +0900

    백준2문제

---
+++ src/BaekJoon/_2738_행렬덧셈.java
@@ -0,0 +1,21 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class _2738_행렬덧셈 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int h = sc.nextInt();
+		int r = sc.nextInt();
+		
+		int [][] hang = new int[h][r];
+		int [][] hang2 = new int [h][r];
+				
+		for(int i = 0; i < h ; i++) {
+			
+		}
+		
+	}
+
+}
