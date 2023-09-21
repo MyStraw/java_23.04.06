2c85c3dd33c3563feacf24de97cdf56ca0224639
MyStraw <kikongdosa@gmail.com>
2023-09-21 09:35:26 +0900

    0921

--- src/BaekJoon_notyet/_5337_웰컴.java
+++ src/BaekJoon_notyet/_5337_웰컴.java
@@ -2,8 +2,8 @@ package BaekJoon_notyet;
 
 public class _5337_웰컴 {
 	public static void main(String[] args) {
-		System.out.println(".  .   .\r\n"
-				+ "|  | _ | _. _ ._ _  _\r\n"
+		System.out.println(".  .   .\n"
+				+ "|  | _ | _. _ ._ _  _\n"
 				+ "|/\\|(/.|(_.(_)[ | )(/.");	
 	}
 }
