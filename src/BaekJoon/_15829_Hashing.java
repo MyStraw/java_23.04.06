64914226cb2c71a6b7b994b4fa426a91fef8fc34
MyStraw <kikongdosa@gmail.com>
2023-09-26 09:18:29 +0900

    0926

---
+++ src/BaekJoon/_15829_Hashing.java
@@ -0,0 +1,23 @@
+package BaekJoon;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class _15829_Hashing {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int L = sc.nextInt();
+		String alpha = sc.next();			
+		BigInteger total = BigInteger.ZERO;		
+		BigInteger base = BigInteger.valueOf(31);
+		BigInteger mod = BigInteger.valueOf(1234567891);
+		
+		for (int i = 0 ; i<L ; i++) {
+			int value = (int) alpha.charAt(i) - 96;			
+			BigInteger pow = BigInteger.valueOf(value).multiply(base.pow(i));			
+			total = total.add(pow);
+		}
+		System.out.println(total.mod(mod));
+	}
+}
