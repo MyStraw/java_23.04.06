34a6ea8337b42aba9cfdbcbfb07569c9058ce219
MyStraw <kikongdosa@gmail.com>
2023-10-05 13:01:49 +0900

    10.05 백준풀이

---
+++ src/BaekJoon/_15964_이상한기호.java
@@ -0,0 +1,18 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class _15964_이상한기호 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		long A = sc.nextLong();
+		long B = sc.nextLong();
+		
+		long total = (A+B)*(A-B);
+		
+		System.out.println(total);
+	}
+
+}
