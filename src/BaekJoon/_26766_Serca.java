db53326a1fb24991147ff0de67e50d53b77709e8
MyStraw <kikongdosa@gmail.com>
2023-11-01 13:23:40 +0900

    11-01

---
+++ src/BaekJoon/_26766_Serca.java
@@ -0,0 +1,24 @@
+package BaekJoon;
+
+import java.util.Scanner;
+
+public class _26766_Serca {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		
+		for (int i = 0 ; i<n ; i++) {
+			System.out.println(" @@@   @@@ \n"
+					+ "@   @ @   @\n"
+					+ "@    @    @\n"
+					+ "@         @\n"
+					+ " @       @ \n"
+					+ "  @     @  \n"
+					+ "   @   @   \n"
+					+ "    @ @    \n"
+					+ "     @     "
+					);
+			
+		}
+	}
+}
