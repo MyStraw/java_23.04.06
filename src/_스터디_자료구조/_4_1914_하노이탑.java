4e07391595233f30e775ac6ae77e0e463c27ddf7
MyStraw <kikongdosa@gmail.com>
2023-11-02 17:22:46 +0900

    feat 11-02

--- src/_스터디_자료구조/_4_1914_하노이탑.java
+++ src/_스터디_자료구조/_4_1914_하노이탑.java
@@ -1,25 +1,33 @@
 package _스터디_자료구조;
 
+import java.io.IOException;
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class _4_1914_하노이탑 {
-
+	
 	static StringBuffer str = new StringBuffer();
-	static int K = 0;
 
-	public static void main(String[] args) {
+	static int K = 0;
+	
+	public static void main(String[] args) throws IOException {		
 		Scanner sc = new Scanner(System.in);
-		int N = sc.nextInt();
-		Hanoi(N, 1, 2, 3);
-		if (N < 20) {
+		int N = sc.nextInt();		
+		if (N <= 20) {
+			Hanoi(N, 1, 2, 3);
 			System.out.println(K);
 			System.out.println(str);
-		} else if (N >= 20) {
-			System.out.println(K);
+			
+		} else if (N > 20) {			
+//			BigInteger a = BigInteger.valueOf(2);
+//			BigInteger b = a.pow(N);
+//			BigInteger c = BigInteger.ONE;
+//			BigInteger d = b.subtract(c);
+			System.out.println(new BigInteger("2").pow(N).subtract(BigInteger.ONE));
 		}
 	}
 
-	public static void Hanoi(int Num, int from, int by, int to) {
+	public static void Hanoi(int Num, int from, int by, int to) throws IOException {
 		if (Num == 1) {
 			str.append(from + " " + to).append("\n");
 			K++;
---
+++ src/_스터디_자료구조/_4_9663_N_Queen.java
@@ -0,0 +1,8 @@
+package _스터디_자료구조;
+
+public class _4_9663_N_Queen {
+	public static void main(String[] args) {
+		
+	}
+
+}
