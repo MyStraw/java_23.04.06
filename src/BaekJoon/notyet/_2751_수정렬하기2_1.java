f4ce0e9ff2cc5e92200d099d5d849f3e638fc654
MyStraw <kikongdosa@gmail.com>
2023-11-04 20:17:42 +0900

    1104 백준

--- src/BaekJoon/notyet/_2751_수정렬하기2_1.java
+++ src/BaekJoon/notyet/_2751_수정렬하기2_1.java
@@ -5,52 +5,43 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.Scanner;
+import java.util.ArrayList;
 
-public class _2751_수정렬하기2 {
+public class _2751_수정렬하기2_1 {
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));		
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));	
 		
-		Scanner sc = new Scanner(System.in);
-
 		int N = Integer.parseInt(br.readLine()) ;
-		int[] su = new int[N];
-
+		ArrayList<Integer> suu = new ArrayList<>();
+		
 		for (int i = 0; i < N; i++) {
-			su[i] = Integer.parseInt(br.readLine());
+			suu.add(i, Integer.parseInt(br.readLine()));
 		}
+
 
 		for (int i = 0; i < N; i++) {
 			for (int j = 0; j < N - 1; j++) {
-				if (su[j] > su[j + 1]) {
-					swap(su, j, j + 1);
+				if (suu.get(j) > suu.get(j+1)) {
+					swap(suu, j, j + 1);
 				}
-
 			}
-
 		}
 
-		for (int r : su) {
+		for (int r : suu) {
 			bw.write(Integer.toString(r)+ "\n");
 		}
 		bw.flush();
 		bw.close();
 	}
 
-	private static void swap(int[] su, int i, int j) {
-		int temp = su[i];
-		su[i] = su[j];
-		su[j] = temp;
+	private static void swap(ArrayList<Integer> suu, int i, int j) {
+		int temp = suu.get(i);		
+		suu.set(i, suu.get(j));
+		suu.set(j, temp);
 	}
 
 }
 
-
-//ArrayList<Integer> suu = new ArrayList<>();
-//
-//for (int i = 0; i < N; i++) {
-//	suu.add(i, Integer.parseInt(br.readLine()));
-//}
 
---
+++ src/_스터디_자료구조/_5_문제_1517_버블소트.java
@@ -0,0 +1,60 @@
+package _스터디_자료구조;
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class _5_문제_1517_버블소트 {
+	static int count = 0;
+
+	static int bubbleSort(int[] bubble, int dataSize) {
+		int k = 0;
+		while (true) {
+			int exchange = 0;
+			int last = dataSize - 1;
+			for (int j = dataSize - 1; j > k; j--) { // 젤 오른쪽부터
+				if (bubble[j] < bubble[j - 1]) { // 오름차순
+					swap(bubble, j - 1, j);
+					count++;
+					exchange++;
+					last = j;
+				}
+			}
+			if (exchange == 0)
+				break;
+			k = last;
+		}
+		return count;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		int dataSize = Integer.parseInt(st.nextToken());
+		int[] bubble = new int[dataSize];
+		StringTokenizer data = new StringTokenizer(br.readLine());
+
+		for (int i = 0; i < dataSize;) {
+			bubble[i] = Integer.parseInt(data.nextToken());
+			i++;
+		}
+
+		bubbleSort(bubble, dataSize);
+		bw.write(String.valueOf(count));
+		bw.flush();
+		bw.close();
+	}
+	
+	static void swap(int[] bubble, int index1, int index2) {
+		int temp = bubble[index1];
+		bubble[index1] = bubble[index2];
+		bubble[index2] = temp;
+	}
+
+}
